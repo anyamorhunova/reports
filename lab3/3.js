444697aa61c95a434ed8cdd91dd2b1d77d594f38
Anya Morhunova <fyz12vjhueyjdf@gmail.com>
2020-10-15 16:40:18 +0300

    first commit

---
+++ lab3/3.js
@@ -0,0 +1,42 @@
+function view1(){
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+
+
+
+}
+
+function view2(){
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div2").style.display = "block";
+    document.getElementById("div1").style.display = "none";
+
+}
+
+
+function view(){
+    document.getElementById("a1").style.display = "block";
+    document.getElementById("a2").style.display = "block";
+}
+
+function op1(){
+    document.getElementById("div11").style.display = "block";
+    document.getElementById("div12").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+
+}
+
+function op2(){
+    document.getElementById("div12").style.display = "block";
+    document.getElementById("div11").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+}
+
+
+
+
